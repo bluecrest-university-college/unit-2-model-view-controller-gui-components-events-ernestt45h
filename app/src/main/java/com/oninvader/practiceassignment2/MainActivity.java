074cfe98409e725c78966b1add03b6b96d289018
Ernest <ernest.hayford@bluecrest.edu.gh>
2019-03-24 10:46:51 +0000

    Done

--- app/src/main/java/com/oninvader/practiceassignment2/MainActivity.java
+++ app/src/main/java/com/oninvader/practiceassignment2/MainActivity.java
@@ -2,12 +2,41 @@ package com.oninvader.practiceassignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    TextView label;
+    Button button;
+    String verified;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email);
+        label = (TextView) findViewById(R.id.label);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verified = email.getText().toString();
+                if(verified.contains("@")){
+                    label.setText("Verified");
+                    label.setVisibility(View.VISIBLE);
+                    label.setTextColor(getResources().getColor(R.color.success));
+                }else{
+                    label.setText("Declined");
+                    label.setVisibility(View.VISIBLE);
+                    label.setTextColor(getResources().getColor(R.color.error));
+                }
+            }
+        });
     }
 }
